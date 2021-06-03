f92369581042c6a595eee6a92366cee6bdd86775
Ncesecrede <francescopm95@gmail.com>
2021-06-03 14:59:25 +0100

    Lista Cognomi

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-var lista = ["Pagano", "Monti", "Coraggio"];
+var lista = ["Pagano", "Viscardi", "Coraggio"];
 var cognome = prompt("inserisci cognome");
-var x = lista.indexOf(cognome);
+var position = lista.indexOf(cognome);
 
 while (lista.length < 4) {
     lista.push(cognome);
 }
-document.getElementById("result").innerHTML = lista.sort() + x;
+
+document.getElementById("result").innerHTML = lista.sort();
+

8be982e8919c30d6795b65585cf8b75419000668
Ncesecrede <francescopm95@gmail.com>
2021-06-03 16:03:48 +0100

    Lista Cognomi

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
 var lista = ["Pagano", "Viscardi", "Coraggio"];
 var cognome = prompt("inserisci cognome");
+
+lista.push(cognome);
 var position = lista.indexOf(cognome);
 
-while (lista.length < 4) {
-    lista.push(cognome);
-}
-
-document.getElementById("result").innerHTML = lista.sort();
-
+console.log(lista.sort() + position.sort())
